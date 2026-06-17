0e73387fae093989f69c0705fceee68bfb9afcd0
malhotraakhil <malhotraakhil@hotmail.com>
2026-06-17 23:47:57 +0530

    DataTypesPri-2

---
+++ PrimitiveDTs.java
@@ -0,0 +1,23 @@
+
+public class PrimitiveDTs{
+	public static void main(String [] args){
+		// Scanner obj= new Scanner(System.in);
+		int BasicInt= 100;
+		long LongNum= 3894619183149846L;
+		char Charss= 'r';
+		float DecimalNum= 123.45f;
+		double BigDecimal= 470141830801.404937441930;
+		
+		
+		System.out.println("Primitive datatypes:");
+		//DT is a type that that will not change its DT
+		//when it is broken down
+		
+		System.out.println("Integer: "+ BasicInt);
+		System.out.println("Long integer: "+ LongNum);
+		System.out.println("Character "+ Charss);
+		System.out.println("Float "+ DecimalNum);
+		System.out.println("Long float "+ BigDecimal);
+		
+}
+}
