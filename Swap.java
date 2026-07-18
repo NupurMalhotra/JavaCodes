8fe7d69318ba3a0fcaa6ac477090fc3de057b70b
malhotraakhil <malhotraakhil@hotmail.com>
2026-07-18 17:58:10 +0530

    internal working in swapping

---
+++ Swap.java
@@ -0,0 +1,14 @@
+public class Swap{
+	//method
+	static void textMethod(String message)
+	{
+		message= "hello world";
+		System.out.println(message);
+		// return message;
+	}
+	public static void main(String []args){
+		String name= "nupur malhotra";
+		System.out.println("answer?"+textMethod(message));
+	}
+}
+		
---
+++ Swap2.java
@@ -0,0 +1,27 @@
+// class Test2{
+	// static String Method2( String intro){
+		// intro= "hello world";
+		// return intro;
+	// }
+// }
+	
+// class Swap2 extends Test2{
+	// public static void main(String[]args){
+		// Test2 obj= new Test2();
+		// String name= "nupur malhotra";
+		// System.out.println(obj.Method2(name));
+	// }
+// }
+	
+	class Swap2{
+		public static void main(String[]args){
+			String name= "nupur";
+			method2(name);
+			System.out.println(name);
+		}
+	    static void method2( String intro){
+		    intro= "hello world";
+		
+		    
+	}
+}
