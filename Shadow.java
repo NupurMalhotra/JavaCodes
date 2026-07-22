cd475291e0c3423feb92f14bee820014feeb2da6
malhotraakhil <malhotraakhil@hotmail.com>
2026-07-22 23:49:44 +0530

    shadowing a scope

---
+++ Shadow.java
@@ -0,0 +1,12 @@
+public class Shadow{
+	static int a= 20;
+	public static void main(String []args){
+		int a= 30;
+		System.out.println(a);
+	}
+	
+	static void method(){
+		System.out.println(a);
+	}
+	// System.out.println(a);
+}
